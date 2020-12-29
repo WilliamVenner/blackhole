971131a13c662755206d05da90d1ec35c95f6591
Will Venner <william@venner.io>
2020-12-29 02:39:25 +0000

    Suppress command prompt + PowerShell windows, implement SHChangeNotify to refresh the folder + quick access icons

--- src/windows.rs
+++ src/windows.rs
@@ -1,15 +1,28 @@
 use crate::Blackhole;
 use crate::Show;
 
-use std::{iter, fs, os::windows::ffi::OsStrExt, process::Command};
+use std::{ffi::CString, fs, iter, os::windows::ffi::OsStrExt, os::windows::process::CommandExt, process::Command};
 use ini::{Ini, EscapePolicy};
 use winreg::{enums::*, RegKey};
-use winapi;
+use winapi::{self, um::winbase::CREATE_NO_WINDOW};
+
+// Bind SHChangeNotify
+extern "system" {
+	pub fn SHChangeNotify(
+		wEventId: winapi::um::winnt::LONG,
+		uFlags: winapi::shared::minwindef::UINT,
+		dwItem1: winapi::shared::minwindef::LPCVOID,
+		dwItem2: winapi::shared::minwindef::LPCVOID
+	);
+}
+const SHCNE_UPDATEDIR: winapi::um::winnt::LONG = 0x00001000;
+const SHCNF_PATHW: winapi::shared::minwindef::UINT = 0x0005;
 
 pub trait Windows {
 	fn powershell(script: String);
+	fn change_notify(ptr: *const winapi::ctypes::c_void);
+	fn set_file_attributes(path: &std::path::PathBuf, attr: winapi::shared::minwindef::DWORD) -> *const u16;
 	fn quick_access(path: &std::path::PathBuf);
-	fn set_file_attributes(path: &std::path::PathBuf, attr: winapi::shared::minwindef::DWORD);
 	fn set_blackhole_attributes(path: &std::path::PathBuf);
 	fn edit_startup_registry();
 	fn chores(&self);
@@ -19,26 +32,53 @@ impl Windows for Blackhole {
 	// Run a PowerShell script
 	// TODO: Replace these with winapi calls when the relevant functions are binded
 	fn powershell(script: String) {
-		Command::new("powershell").args(&["-Command", &script]).status().ok();
+		match Command::new("powershell").args(&["-Command", &script])
+		.creation_flags(CREATE_NO_WINDOW)
+		.stderr(std::process::Stdio::piped())
+		.stdout(std::process::Stdio::null())
+		.stdin(std::process::Stdio::null())
+		.output() {
+			Err(err) => { Show::error(&format!("Error executing PowerShell script: {}", err)) },
+			Ok(output) => {
+				if output.stderr.len() > 0 {
+					Show::error(&format!("PowerShell error:\n{}", String::from_utf8(output.stderr).unwrap_or_else(|_| String::from("String conversion error"))));
+				}
+			}
+		};
+	}
+
+	// SHChangeNotify(SHCNE_UPDATEDIR, ...) tell a file or directory to update its icon
+	fn change_notify(ptr: *const winapi::ctypes::c_void) {
+		unsafe {
+			SHChangeNotify(SHCNE_UPDATEDIR, SHCNF_PATHW, ptr, std::ptr::null_mut());
+		}
+	}
+
+	// Sets Windows file attributes
+	fn set_file_attributes(path: &std::path::PathBuf, attr: winapi::shared::minwindef::DWORD) -> *const u16 {
+		let path_utf16: Vec<u16> = path.as_os_str().encode_wide().chain(iter::once(0)).collect();
+		let path_utf16_ptr = path_utf16.as_ptr();
+		unsafe {
+			winapi::um::fileapi::SetFileAttributesW(path_utf16_ptr, attr);
+		}
+		return path_utf16_ptr;
 	}
 
 	// Add $BLACKHOLE to the Quick Access tab
 	fn quick_access(path: &std::path::PathBuf) {
 		Blackhole::powershell(format!("$o = new-object -com shell.application\n$o.Namespace('{}').Self.InvokeVerb('pintohome')", path.display()));
-	}
 
-	// Sets Windows file attributes
-	fn set_file_attributes(path: &std::path::PathBuf, attr: winapi::shared::minwindef::DWORD) {
-		let path_utf16: Vec<u16> = path.as_os_str().encode_wide().chain(iter::once(0)).collect();
-		unsafe {
-			winapi::um::fileapi::SetFileAttributesW(path_utf16.as_ptr(), attr);
-		}
+		let quick_access_folder_path = match CString::new("shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}") {
+		    Ok(quick_access_folder_path) => quick_access_folder_path,
+		    Err(err) => { Show::error(&format!("String conversion error: {}", err)); return; }
+		};
+		Blackhole::change_notify(quick_access_folder_path.as_ptr() as *const _);
 	}
 	
 	// Configures the $BLACKHOLE folder
 	fn set_blackhole_attributes(path: &std::path::PathBuf) {
 		// Set the file attributes of the blackhole directory itself
-		Blackhole::set_file_attributes(path, winapi::um::winnt::FILE_ATTRIBUTE_SYSTEM | winapi::um::winnt::FILE_ATTRIBUTE_READONLY);
+		let path_utf16_ptr = Blackhole::set_file_attributes(path, winapi::um::winnt::FILE_ATTRIBUTE_SYSTEM | winapi::um::winnt::FILE_ATTRIBUTE_READONLY);
 
 		// Create desktop.ini
 		let mut ini = path.to_owned();
@@ -89,6 +129,8 @@ impl Windows for Blackhole {
 				return;
 			}
 		}
+		
+		Blackhole::change_notify(path_utf16_ptr as *const _);
 	}
 
 	fn edit_startup_registry() {
